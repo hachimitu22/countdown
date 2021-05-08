4727b1e935f6b66652e04f6874238cea427c177f
hachimitu22 <hachimitu22.888@gmail.com>
2021-05-09 02:06:14 +0900

    CountdownクラスはFlowクラスでフローを制御する

--- src/countdown.ts
+++ src/countdown.ts
@@ -1,24 +1,23 @@
 import IGame from './game/IGame';
-import NormalGame from './game/normal-game';
 import Timer from './timer/timer';
 import Random from './random/random';
 import Sound from './sound/sound';
+import GameFlow from './flow/game-flow';
 
 export default class Countdown {
-  private game: IGame;
-  constructor() {
-    this.game = new NormalGame(
-      10,
-      new Timer(),
-      new Random(),
-      new Sound('./voice/'),
-    );
-  }
+  constructor() { }
   async execute(): Promise<void> {
     try {
-      while (!this.game.isFinish()) {
-        await this.game.play();
-        this.game = this.game.next();
+      const flow: GameFlow = new GameFlow(
+        new Timer(),
+        new Random(),
+        new Sound('./voice/'),
+      );
+      let game: IGame = flow.firstGame();
+
+      while (!flow.isFinish()) {
+        await game.play();
+        game = flow.nextGame(game);
       }
       return Promise.resolve();
     } catch (err) {
