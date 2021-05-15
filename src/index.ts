769bb14830326b71b0ab2541616ab33413318b4c
hachimitu22 <hachimitu22.888@gmail.com>
2021-05-16 04:05:30 +0900

    ゲーム中にエンターキーで降参する

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import Countdown from './countdown';
 import Random from './random/random';
 import Sound from './sound/sound';
 import Timer from './timer/timer';
+import readline from 'readline';
 
 const countdown = new Countdown(
   new Timer(),
   new Random(),
   new Sound('./voice/'),
 );
+const input = readline.createInterface(process.stdin, process.stdout);
+
 countdown.execute()
   .catch(err => {
     console.log(err);
+  })
+  .finally(() => {
+    input.close();
   });
+
+input.once('line', (_) => {
+  input.close();
+  countdown.surrender();
+  console.log('surrender');
+});
