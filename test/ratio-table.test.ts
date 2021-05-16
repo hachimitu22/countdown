38d2e5ce61c66895f8db03775869026d9bfabc06
hachimitu22 <hachimitu22.888@gmail.com>
2021-05-16 09:55:01 +0900

    RatioTable のテストを追加

---
+++ test/ratio-table.test.ts
@@ -0,0 +1,39 @@
+import * as chai from 'chai';
+import RatioRecord from '../src/ratio/ratio-record';
+import RatioTable from '../src/ratio/ratio-table';
+import FixRandom from './fix-random';
+
+describe('RatioTable', () => {
+  const random: FixRandom = new FixRandom([]);
+  const table = new RatioTable<string>([
+    new RatioRecord<string>('cat', 100),
+    new RatioRecord<string>('dog', 100),
+    new RatioRecord<string>('pig', 100),
+  ]);
+
+  it('抽選値1-100で"cat"が抽選される', () => {
+    random.add(1);
+    chai.expect(table.lot(random)).to.equal('cat');
+    random.add(100);
+    chai.expect(table.lot(random)).to.equal('cat');
+  });
+
+  it('抽選値101-200で"dog"が抽選される', () => {
+    random.add(101);
+    chai.expect(table.lot(random)).to.equal('dog');
+    random.add(200);
+    chai.expect(table.lot(random)).to.equal('dog');
+  });
+
+  it('抽選値201-300で"pig"が抽選される', () => {
+    random.add(201);
+    chai.expect(table.lot(random)).to.equal('pig');
+    random.add(300);
+    chai.expect(table.lot(random)).to.equal('pig');
+  });
+
+  it('抽選値301以上で例外が発生する', () => {
+    random.add(301);
+    chai.expect(() => table.lot(random)).to.throw();
+  });
+});
