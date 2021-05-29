a9645cfede55dadf55ef5a01bab6a5b792192e86
hachimitu22 <hachimitu22.888@gmail.com>
2021-05-29 19:16:37 +0900

    Soundのテストを通す

--- test/sound.test.ts
+++ test/sound.test.ts
@@ -1,16 +1,18 @@
 import * as chai from 'chai';
 import Sound from '../src/frontend/sound/sound';
 
+const voiceDirectory = './public/voice';
+
 describe('Sound', () => {
   it('音声が再生される', (done) => {
-    const sound = new Sound('./voice');
+    const sound = new Sound(voiceDirectory);
     sound.play('1.wav')
       .then(done)
       .catch(done);
   }).timeout(5000);
 
   it('2つの音声が同時に再生される', (done) => {
-    const sound = new Sound('./voice');
+    const sound = new Sound(voiceDirectory);
 
     Promise.all([
       sound.play('1.wav'),
@@ -21,7 +23,7 @@ describe('Sound', () => {
   }).timeout(5000);
 
   it('音声が中断される', (done) => {
-    const sound = new Sound('./voice');
+    const sound = new Sound(voiceDirectory);
     sound.play('finish.wav')
       .then(() => {
         return new Promise((resolve) => setTimeout(resolve, 500));
