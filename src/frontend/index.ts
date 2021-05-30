425476f6a9bddc541a97b81c6bc1dd8ba9039c0a
hachimitu22 <hachimitu22.888@gmail.com>
2021-05-30 18:55:04 +0900

    htmlでカウントダウンが遊べるようにする

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,31 +1,54 @@
 import Countdown from './countdown';
 import Random from './random/random';
-import Sound from './sound/sound';
+import AudioSound from './sound/audio-sound';
 import Timer from './timer/timer';
-import readline from 'readline';
 
-const countdown = new Countdown(
-  new Timer(),
-  new Random(),
-  new Sound('./voice/'),
-);
-const input = readline.createInterface(process.stdin, process.stdout);
+window.onload = () => {
+  const mainElement = document.getElementById('main');
+  const audioElement = document.getElementById('voice');
 
-const start = Date.now();
+  if (!mainElement) {
+    console.log('not exist id main');
+    return;
+  }
+  if (!audioElement) {
+    console.log('not exist id voice');
+    return;
+  }
+  if (!(audioElement instanceof HTMLAudioElement)) {
+    console.log('id voice is not HTMLAudioElement');
+    return;
+  }
 
-countdown.execute()
-  .then(() => {
-    countdown.gameOver();
-  })
-  .catch(err => {
-    console.log(err);
-  })
-  .finally(() => {
-    input.close();
-  });
+  mainElement.addEventListener('click', (() => {
+    const start = () => {
+      const countdown = new Countdown(
+        new Timer(),
+        new Random(),
+        new AudioSound('./voice/', audioElement),
+      );
 
-input.once('line', (_) => {
-  input.close();
-  countdown.surrender();
-  console.log('surrender');
-});
+      countdown.execute()
+        .then(() => {
+          countdown.gameOver();
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => {
+        });
+
+      mainElement.removeEventListener('click', start);
+
+      const surrender = () => {
+        countdown.surrender();
+        mainElement.removeEventListener('click', surrender);
+        mainElement.addEventListener('click', start);
+      };
+      mainElement.addEventListener('click', surrender);
+    };
+
+    return start;
+  })());
+
+};
